3875b9ec186167001109d95f3b6bd08220a58eda
ayman <aymanimtyaz@gmail.com>
2021-01-07 19:15:23 +0530

    add ISOLATION_LEVEL_AUTOCOMMIT

--- sql_terminal.py
+++ sql_terminal.py
@@ -1,14 +1,18 @@
 import psycopg2 as pg2 
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from getpass import getpass
 
 class connection:
 
-    def __init__(self, database, user, password, host = 'localhost', port = '5432'):
+    def __init__(self, database, user, password, iso_level, host = 'localhost', port = '5432'):
         self.database = database
         self.user = user
         self.password = password
         self.host = host
+        self.iso_level = iso_level
         self.con = pg2.connect(database = self.database, host = self.host, user = self.user, password = self.password)
+        if self.iso_level == 'y':
+            self.con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
     def close_con(self):
         self.con.close()
@@ -22,16 +26,21 @@ def get_connection_info():
     port = input('Enter port: ')
     usr = input('Enter user: ')
     pw = getpass('Enter password: ')
+    isolation_bool = ''
+    while True:
+        isolation_bool = input('Run with autocommit? (y/n): ')
+        if isolation_bool == 'y' or isolation_bool == 'n':
+            break
     print('\nConnecting to database...')
 
     if host == '' and port == '':
-        conn = connection(database = dbname, user = usr, password = pw)
+        conn = connection(database = dbname, user = usr, password = pw, iso_level = isolation_bool)
     elif port == '' and host != '':
-        conn = connection(database = dbname, user = usr, password = pw, host = host)
+        conn = connection(database = dbname, user = usr, password = pw, iso_level = isolation_bool, host = host)
     elif host == '' and port != '':
-        conn = connection(database = dbname, user = usr, password = pw, port = port)
+        conn = connection(database = dbname, user = usr, password = pw, iso_level = isolation_bool, port = port)
     else:
-        conn = connection(database = dbname, user = usr, password = pw, host = host, port = port)
+        conn = connection(database = dbname, user = usr, password = pw, iso_level = isolation_bool, host = host, port = port)
 
     return conn
 
