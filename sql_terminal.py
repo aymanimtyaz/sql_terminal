b3d81909a2c06ade0b6b64e7d63d2c7bcdd9a4c9
ayman <aymanimtyaz@gmail.com>
2021-01-11 23:56:29 +0530

    Add SSH tunneling functionality to access remote databases

--- sql_terminal.py
+++ sql_terminal.py
@@ -1,8 +1,31 @@
 import psycopg2 as pg2 
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from getpass import getpass
-import sys, os
+import sys, os, json
 from tabulate import tabulate
+
+hostname = ''
+dbport = ''
+ssh_connection = None
+
+class ssh_con:
+
+    def __init__(self, host, port, user, password = None, private_key = None, remote_bind_address = None):
+        import sshtunnel
+        from sshtunnel import SSHTunnelForwarder as stf
+        self.host = host
+        self.port = port
+        self.user = user
+        self.password = password
+        self.private_key = private_key
+        self.remote_bind_address = remote_bind_address
+        self.tunnel = stf(ssh_address_or_host = self.host, ssh_port = self.port, ssh_username = self.user, ssh_pkey = self.private_key,
+             remote_bind_address = self.remote_bind_address)
+        self.tunnel.start()
+        print(f'SSH connection established to \u001b[32m{self.host}:\u001b[36m{self.port}\u001b[0m')
+
+    def disconnect_ssh(self):
+        self.tunnel.close()
 
 class connection:
 
@@ -21,6 +44,47 @@ class connection:
     def close_con(self):
         self.con.close()
 
+def establish_ssh_tunnel():
+    global ssh_connection
+    if len(sys.argv) > 1:
+        for argument in sys.argv[1:]:
+            if os.path.splitext(argument)[1] == '.json':
+                with open(argument) as config_json:
+                    config = json.loads(config_json.read())
+                    host = config['ssh_host']
+                    port = config['ssh_port']
+                    user = config['ssh_username']
+                    password = config['ssh_password']
+                    if password == "":
+                        password = None
+                    private_key = config["ssh_pkey"]
+                    remote_host = config['host']
+                    remote_port = int(config['port'])
+                    remote_bind_address = (remote_host, remote_port)
+                    ssh_connection = ssh_con(host, port, user, password, private_key, remote_bind_address)
+                    return ssh_connection
+    global hostname; global dbport
+    while True:
+        host = input("Enter the hostname/ip of the server to SSH to: ")
+        if host != '':
+            break
+    port = input("Enter port of the server to SSH to (default 22): ")
+    if port == '':
+        port = 22
+    while True:
+        user = input("Enter the username to SSH in as: ")
+        if user != '':
+            break
+    password = input("Enter the password if required: ")
+    if password == '':
+        password = None
+    private_key = input("Enter the path to the private key if using a private key: ")
+    if private_key == '':
+        private_key = None
+    remote_bind_address = (hostname, int(dbport))
+    ssh_connection = ssh_con(host, port, user, password, private_key, remote_bind_address)
+    return ssh_connection
+             
 def execute_from_file(curs, argument):
     sql = open(argument).read()
     print('EXECUTING FROM FILE...\n')
@@ -31,11 +95,13 @@ def execute_from_file(curs, argument):
 
 def exit_routine(conn):
     conn.close_con()
+    global ssh_connection
+    if ssh_connection is not None:
+        ssh_connection.disconnect_ssh()
     print('\n') 
     sys.exit(0)
 
 def get_from_cli_arguments(argument):
-    import json
     credentials = json.loads(open(argument).read())
     host = credentials["host"]
     port = credentials["port"]
@@ -53,6 +119,8 @@ def get_from_cli_arguments(argument):
 def get_interactive():
     host = input('Enter host (default: localhost): ')
     port = input('Enter port (default: 5432): ')
+    if port == '':
+        port = '5432'
     dbname = ''; usr = ''; pw = ''; isolation_bool = ''
     while True:
         dbname = input('Enter database name: ')
@@ -70,31 +138,44 @@ def get_interactive():
         isolation_bool = input('Run with autocommit? (y/n): ')
         if isolation_bool == 'y' or isolation_bool == 'n':
             break
-    
+
     return host, port, dbname, usr, pw, isolation_bool
 
 def get_connection_info():
+    global hostname; global dbport
 
     print('\n******************************* \u001b[32mSQL TERMINAL\u001b[0m *******************************')
     creds_not_validated = True
     for argument in sys.argv[1:]:
         if os.path.splitext(argument)[1] == '.json':
-            host, port, dbname, usr, pw, isolation_bool = get_from_cli_arguments(argument)
+            hostname, dbport, dbname, usr, pw, isolation_bool = get_from_cli_arguments(argument)
             creds_not_validated = False
             break
     if creds_not_validated:
-        host, port, dbname, usr, pw, isolation_bool = get_interactive()
+        hostname, dbport, dbname, usr, pw, isolation_bool = get_interactive()
+
+    ssh_mode = ''
+
+    while True:
+        ssh_mode = input('Would you like to connect through an SSH tunnel? (y/n): ')
+        if ssh_mode == 'y' or ssh_mode == 'n':
+            break
+
+    if ssh_mode == 'y':
+        ssh_tunnel = establish_ssh_tunnel()
+        conn = connection(database = dbname, user = usr, password = pw, iso_level = isolation_bool, host = 'localhost', port = ssh_tunnel.tunnel.local_bind_port)
+        return conn
 
     print('\nConnecting to database...')
 
-    if host == '' and port == '':
+    if hostname == '' and dbport == '':
         conn = connection(database = dbname, user = usr, password = pw, iso_level = isolation_bool)
-    elif port == '' and host != '':
-        conn = connection(database = dbname, user = usr, password = pw, iso_level = isolation_bool, host = host)
-    elif host == '' and port != '':
-        conn = connection(database = dbname, user = usr, password = pw, iso_level = isolation_bool, port = port)
+    elif dbport == '' and hostname != '':
+        conn = connection(database = dbname, user = usr, password = pw, iso_level = isolation_bool, host = hostname)
+    elif hostname == '' and dbport != '':
+        conn = connection(database = dbname, user = usr, password = pw, iso_level = isolation_bool, port = dbport)
     else:
-        conn = connection(database = dbname, user = usr, password = pw, iso_level = isolation_bool, host = host, port = port)
+        conn = connection(database = dbname, user = usr, password = pw, iso_level = isolation_bool, host = hostname, port = dbport)
 
     return conn
 
